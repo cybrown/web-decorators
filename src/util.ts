ff7fea5bb64ca75180bf5e4ccee7fee2c99273c6
Cy Brown <cy.brown59@gmail.com>
2015-04-12 03:38:39 +0200

    Added Optional

--- src/util.ts
+++ src/util.ts
@@ -9,3 +9,72 @@ export function unwrapAsyncValue<T>(value: any, cb: (err: any, value: T) => void
         }
     }).catch(err => cb(err, null));
 };
+
+export class Optional<T> {
+
+    private _value: T;
+
+    constructor (value: T) {
+        if (value == null) {
+            if (Optional.absent == null) {
+                Optional.absent = this;
+            }
+            return Optional.absent;
+        } else {
+            this._value = value;
+        }
+    }
+
+    get isEmpty(): boolean {return this._value == null;}
+    get isPresent(): boolean {return !this.isEmpty;}
+
+    get get(): T {
+        if (this.isEmpty) {
+            throw new Error('Can not get of absent');
+        } else {
+            return this._value;
+        }
+    }
+
+    getOrElse(defaultValue: T): T {
+        return this.isEmpty ? defaultValue : this._value;
+    }
+
+    ifPresent(cb: (value: T) => void): Optional<T> {
+        if (!this.isEmpty) {
+            cb(this._value);
+        }
+        return this;
+    }
+
+    orElse(cb: () => void): Optional<T> {
+        if (this.isEmpty) {
+            cb();
+        }
+        return this;
+    }
+
+    map<U>(cb: (value: T) => U): Optional<U> {
+        if (!this.isEmpty) {
+            return Optional.of(cb(this._value));
+        }
+        return Optional.absent;
+    }
+
+    flatMap<U>(cb: (value: T) => Optional<U>): Optional<U> {
+        if (!this.isEmpty) {
+            return cb(this._value);
+        }
+        return Optional.absent;
+    }
+
+    static of<V>(value: V): Optional<V> {
+        if (value == null) {
+            return Optional.absent;
+        } else {
+            return new Optional(value);
+        }
+    }
+
+    static absent = new Optional(null);
+}
--- test/util.spec.ts
+++ test/util.spec.ts
@@ -56,3 +56,71 @@ describe ('unwrapAsyncValue', () => {
         });
     });
 });
+
+describe ('Optional', () => {
+
+    it ('should create an Optional of a value', () => {
+        var maybeOne = new util.Optional(1);
+        assert.equal(maybeOne.get, 1);
+    });
+
+    it ('should throw if get on absent', () => {
+        assert.throws(() => util.Optional.absent.get);
+    });
+
+    it ('Optional.absent should equal new Optional(null)', () => {
+        assert.strictEqual(util.Optional.absent, new util.Optional(null));
+    });
+
+    it ('Optional.absent should equal new Optional(undefined)', () => {
+        assert.strictEqual(util.Optional.absent, new util.Optional(undefined));
+    });
+
+    it ('Optional.absent should equal Optional.of(null)', () => {
+        assert.strictEqual(util.Optional.absent, util.Optional.of(null));
+    });
+
+    it ('Optional.absent should equal Optional.of(undefined)', () => {
+        assert.strictEqual(util.Optional.absent, util.Optional.of(undefined));
+    });
+
+    it ('should call ifPresent if Optional is not absent', done => {
+        util.Optional.of(42).ifPresent(value => {
+            assert.equal(value, 42);
+            done();
+        });
+    });
+
+    it ('should call orElse if Optional is absent', done => {
+        util.Optional.absent.orElse(done);
+    });
+
+    it ('should transform the value when map is called', () => {
+        assert.equal(util.Optional.of(13).map(value => value + 1).get, 14);
+    });
+
+    it ('should transform the value when flatMap is call', () => {
+        assert.equal(util.Optional.of(13).flatMap(value => util.Optional.of(value + 1)).get, 14);
+    });
+
+    it ('should not execute map if value is absent', done => {
+        util.Optional.absent.map(value => {
+            done(true);
+            return value;
+        });
+        done();
+    });
+
+    it ('should return absent if create an optional of null or undefined', () => {
+        assert.equal(new util.Optional(null), util.Optional.absent);
+        assert.equal(new util.Optional(undefined), util.Optional.absent);
+    });
+
+    it ('should return the default value if absent with getOrElse', () => {
+        assert.equal(util.Optional.absent.getOrElse(12), 12);
+    });
+
+    it ('should return the internal value with getOrElse if present', () => {
+        assert.equal(util.Optional.of(42).getOrElse(13), 42);
+    });
+});
